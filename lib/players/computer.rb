26590003b230026b51b30d6a5acb04d63d0c192e
Ni Chia <nichia@gmail.com>
2018-01-28 20:53:33 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -27,7 +27,7 @@ module Players
       num = ai_board(board)
       #puts "> #{num}"
 #<<<<<<< HEAD
-      sleep(0)
+#      sleep(0)
 #=======
       sleep(0.5)
 #>>>>>>> aa60641dfc48eb30d4bf9649158a23db2bb261f4
